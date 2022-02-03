9f51a258c77f6f431a56cf2081e186dc23e5f45b
Yumil <yumilwc@gmail.com>
2022-02-03 21:56:56 +0100

    Script chqnge

--- Assets/Scripts/colision_songs-scripts/colision_tom_low.cs
+++ Assets/Scripts/colision_songs-scripts/colision_tom_low.cs
@@ -2,21 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class colision_tom_low : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public AudioSource tickSource;
-    void Start()
-    {
-        tickSource.GetComponent<AudioSource>();
-    }
-
     private void OnTriggerEnter(Collider infoObjet)
     {
         if (infoObjet.gameObject.tag == "Drumsteaks")
         {
             //jouer le sons de tom low
-            tickSource.Play();
+            GetComponent<AudioSource>().Play();
         }
     }
 }
